7c3b6319d644c31416a866ed15dd0ad81537a0eb
Walther Sean <831636@stma.k12.mn.us>
2022-04-11 12:44:16 -0500

    Template Questions

--- quiz.js
+++ quiz.js
@@ -1,5 +1,7 @@
+var q1 =document.getElementById("a1");
+
 function question1() {
-	if (document.getElementById("answer1").value == "Texas"){
+	if (q1.value == "Texas"){
 	document.getElementById("checkq1").innerHTML = "CORRECT";
 } else {
 	document.getElementById("checkq1").innerHTML = "WRONG";
