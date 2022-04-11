039a91de069cbcb2a6627cc21b679bf6c6109cd6
Walther Sean <831636@stma.k12.mn.us>
2022-04-11 11:57:44 -0500

    Move On

--- seceret.js
+++ seceret.js
@@ -2,13 +2,16 @@
 function password1() {
 	if (document.getElementById("password").value == "1234"){
 	document.getElementById("check").innerHTML = "ACCESS GRANTED"
+	window.location="mainhub.html";
 } else {
 	document.getElementById("check").innerHTML = "ACCESS DENIED"
 }
-	
+function moveon1() {
+		
+	}
 }
 function moveon(){
-        window.location='mainhub.html';
+        
     }
 document.getElementById("verify").addEventListener("click", password1)
 document.getElementById("moveon").addEventListener("click", moveon)
