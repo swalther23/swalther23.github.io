d7431469d42feeb3b31cd312f79c37df9884e536
Walther Sean <831636@stma.k12.mn.us>
2022-04-14 11:52:57 -0500

    Fixed Photo bug

--- JavaScripts/photo.js
+++ JavaScripts/photo.js
@@ -1,6 +1,6 @@
 var b1 = document.getElementById("Mustang");
 var b2 = document.getElementById("TBird");
-var b3 = document.getElementById("F-150");
+var b3 = document.getElementById("F150");
 var b4 = document.getElementById("Chevelle");
 var b5 = document.getElementById("Nova");
 var image = document.getElementById("image1");
@@ -12,10 +12,10 @@ function changeTBird() {
 	image.src = "Images/65ThunderBird";
 }
 function changeF150() {
-	image.src = "Images/79F-150";
+	image.src = "Images/79F150";
 }
 function changeChevelle() {
-	image.src = "Images/69Chevelle";
+	image.src = "69Chevelle";
 }
 function changeNova() {
 	image.src = "Images/69Nova";
